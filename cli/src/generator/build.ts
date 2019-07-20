8b308569599642ff9c33c5dd60f271508c4689a6
Shailen <shailen@gettruck.co.za>
2019-07-20 14:38:14 +0200

    Now generates files and save to disk

--- cli/src/generator/build.ts
+++ cli/src/generator/build.ts
@@ -1,14 +1,42 @@
 import fs from 'fs-extra';
-declare function require(path: string): any;
 
 const getPages = async () => {
-  const pages = await fs.readdir('./pages');
+  const filenames = await fs.readdir(`./pages`);
 
-  return pages;
+  return Promise.all(filenames.map(async filename => ({
+    name: filename,
+    ...await import(`${process.env.PWD}/pages/${filename}`),
+  })));
 };
+
+const getLayouts = async () => {
+  const filenames = await fs.readdir(`./layouts`);
+
+  return Promise.all(filenames.map(async filename => ({
+    name: filename.split('.')[0],
+    ...await import(`${process.env.PWD}/layouts/${filename}`),
+  })));
+};
+
+const generateHTML = (pages) => Promise.all(pages.map(async (page) => ({
+  html: page.layout({ title: page.title, content: page.page(page.data ? await page.data() : {}) }),
+  name: page.name.replace('js', 'html'),
+})));
+
+const mergeLayoutsWithPages = (pages, layouts) => pages
+  .map(({ layout, ...otherValues }) => ({
+    layout: layouts.find(({ name }) => name === layout).default,
+    ...otherValues
+  }));
+
+const saveHTMLToFiles = (pages) => pages.map((page) => fs.writeFile(`${process.env.PWD}/dist/${page.name}`, page.html));
 
 export const builder = async () => {
   const pages = await getPages();
+  const layouts = await getLayouts();
+  const mergedLayoutsWithPages = mergeLayoutsWithPages(pages, layouts);
 
-  console.log(pages);
+  const htmlPages = await generateHTML(mergedLayoutsWithPages);
+
+  saveHTMLToFiles(htmlPages);
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 const html = require('html-template-tag');
+const axios = require('axios');
 
 const page = () => html`
-
+  <p>Hello World</p>
 `;
 
 module.exports = {
