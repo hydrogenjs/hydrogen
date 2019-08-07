0736f80ef660ed62db8117d73b065d5e7b25d27f
Shailen Naidoo <naidooshailen648@gmail.com>
2019-08-07 10:24:33 +0200

    Added detailed build logs

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,7 +1,9 @@
 import { Command, flags } from '@oclif/command';
+import cli from 'cli-ux';
 import { build } from '../services/compiler';
 import { saveHTMLToFiles } from '../services/file';
 import { doFoldersExist } from '../helpers/checkFolder';
+import { logBuildMode } from '../helpers/log';
 
 export class Build extends Command {
   static description = 'Starts building templates for production';
@@ -16,12 +18,20 @@ export class Build extends Command {
   async run(): Promise<boolean|void> {
     const { flags: { dev } } = this.parse(Build);
 
+    await logBuildMode(dev);
+
     if (!await doFoldersExist()) {
       return false;
     }
 
+    cli.action.start('Building files');
+    console.time('Build time');
+
     const htmlPages = await build(dev);
 
     await saveHTMLToFiles(htmlPages);
+
+    cli.action.stop();
+    console.timeEnd('Build time');
   }
 }
