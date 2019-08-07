13b56fe1f04f58d7edcb844d7d2c878d920cbcfc
Shailen Naidoo <naidooshailen648@gmail.com>
2019-08-07 10:37:15 +0200

    Minor changes

--- docs/layouts/default.js
+++ docs/layouts/default.js
@@ -8,7 +8,7 @@ module.exports = async ({ title, content, head }) => html`
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     $${head}
-    ${title ? `<title>${title}</title>` : undefined}
+    $${title ? html`<title>${title}</title>` : ''}
   </head>
   <body>
     <nav>
