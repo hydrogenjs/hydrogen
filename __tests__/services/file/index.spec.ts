57144b9c0ef57a50ebd12eab866a206ca643be83
shailen <shailen@lumkani.com>
2019-09-21 10:37:27 +0200

    Added test for File API > getConfig

--- __tests__/services/file/index.spec.ts
+++ __tests__/services/file/index.spec.ts
@@ -2,8 +2,10 @@ import fs from 'fs-extra';
 
 import copyExtraStaticFiles from '../../../src/services/file/copyExtraStaticFiles';
 import copyStaticFolder from '../../../src/services/file/copyStaticFolder';
+import getConfig from '../../../src/services/file/getConfig';
 
 jest.mock('fs-extra');
+jest.mock('path');
 
 describe('File API', (): void => {
   describe('copyExtraStaticFiles', (): void => {
@@ -17,15 +19,33 @@ describe('File API', (): void => {
   describe('copyStaticFolder', (): void => {
     test('function should return undefined ', async (): Promise<void> => {
       fs.pathExists = jest.fn().mockReturnValue(true);
+
       const res = await copyStaticFolder();
       expect(res).toBeUndefined();
     });
 
     test('function should return false', async (): Promise<void> => {
       fs.pathExists = jest.fn().mockReturnValue(false);
-      const res = await copyStaticFolder();
 
+      const res = await copyStaticFolder();
       expect(res).toBe(false);
+    });
+  });
+
+  describe('getConfig', (): void => {
+    test('function should return empty object', async (): Promise<void> => {
+      fs.pathExists = jest.fn().mockReturnValue(false);
+
+      const res = await getConfig();
+      expect(res).toMatchObject({});
+    });
+
+    test('function should return a module', async (): Promise<void> => {
+      fs.pathExists = jest.fn().mockReturnValue(true);
+
+      const res = await getConfig();
+      expect(typeof res).toBe('object');
+      expect(res).toHaveProperty('default');
     });
   });
 });
