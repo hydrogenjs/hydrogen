37bfba366e9c28b5a96862fb8547b2e19c85e3a1
shailen <shailen@lumkani.com>
2019-09-22 12:18:29 +0200

    Upgraded Head API tests

--- __tests__/services/head/index.spec.ts
+++ __tests__/services/head/index.spec.ts
@@ -4,9 +4,16 @@ import { HeadTag } from '../../../src/services/head/types';
 const head = async (): Promise<HeadTag[]> => [
   ['link', { rel: 'stylesheet', href: '/public/css/main.css' }, false],
   ['script', { src: '/public/js/script.js' }, true],
+  ['script', { type: 'module' }, 'console.log("Hello World")'],
+  ['style', {}, 'p { font-size: 10px }'],
 ];
 
-const expectedOutput = '<link rel="stylesheet" href="/public/css/main.css" /><script src="/public/js/script.js"></script>';
+const expectedOutput = [
+  '<link rel="stylesheet" href="/public/css/main.css" />',
+  '<script src="/public/js/script.js"></script>',
+  '<script type="module">console.log("Hello World")</script>',
+  '<style>p { font-size: 10px }</style>',
+].join('');
 
 describe('Head API', (): void => {
   describe('transformHeadToHTML', (): void => {
