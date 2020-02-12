be65545a74ebedafbf91cc94ef928206ae6d5137
shailen <shailen@lumkani.com>
2020-02-12 16:49:53 +0200

    Now migrating cache

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,20 @@
 const getOldCacheData = async () => {
-  const oldCache = await caches.open(await caches.keys()[0]);
+  const [cacheVersion] = await caches.keys();
 
-  return oldCache.keys();
+  let cacheData = [];
+
+  if (cacheVersion) {
+    const oldCache = await caches.open(cacheVersion);
+    const oldCacheKeys = await oldCache.keys();
+
+    cacheData = Promise.all(oldCacheKeys.map(async request => ({
+      request,
+      response: await oldCache.match(request),
+    })));
+  }
+
+
+  return cacheData;
 };
 
 const removeOldCaches = async () => {
@@ -9,19 +22,22 @@ const removeOldCaches = async () => {
 
   cacheVersions.splice(cacheVersions.indexOf(CACHE_VERSION), 1);
 
-  return Promise.all(cacheVersions.map(cacheName => caches.delete(cacheName)))
+  return Promise.all(cacheVersions.map(cacheName => caches.delete(cacheName)));
 };
 
 self.addEventListener('install', (e) => {
   e.waitUntil(getOldCacheData()
     .then((oldCacheData) => {
-      const requestsToCache = [
-        ...oldCacheData,
-        ...routes.map(({ route }) => new Request(route)),
-      ];
+      const requestsToCache = new Set([
+        ...routes.map(({ route }) => route),
+      ]);
 
       return caches.open(CACHE_VERSION)
-        .then(cache => cache.addAll(requestsToCache));
+        .then((cache) => {
+          const addOldCache = Promise.all(oldCacheData.map(data => cache.put(data.request, data.response)));
+
+          return Promise.all([cache.addAll(requestsToCache), addOldCache]);
+        });
     })
     .then(() => removeOldCaches())
     .catch(console.log));
