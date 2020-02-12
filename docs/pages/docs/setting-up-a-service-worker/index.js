ff785e137bf0de0a305e482da52400257ee3c2ef
shailen <shailen@lumkani.com>
2020-02-12 15:12:10 +0200

    Updated docs

--- docs/pages/docs/setting-up-a-service-worker/index.js
+++ docs/pages/docs/setting-up-a-service-worker/index.js
@@ -121,27 +121,31 @@ const page = () => html`
   <pre>
     <code class="lang-javascript">
       const getOldCacheData = async () => {
-        const oldCache = await caches.open(await caches.keys()[0]).keys()
+        const oldCache = await caches.open(await caches.keys()[0]);
 
-        return oldCache
-      }
+        return oldCache.keys();
+      };
 
       const removeOldCaches = async () => {
-        const cacheVersions = await caches.keys()
-        
-        cacheVersions.splice(cacheVersions.indexOf(CACHE_VERSION), 1)
+        const cacheVersions = await caches.keys();
 
-        for (cacheName of cacheVersions) {
-          await caches.delete(cacheName)
+        cacheVersions.splice(cacheVersions.indexOf(CACHE_VERSION), 1);
+
+        for (const cacheName of cacheVersions) {
+          await caches.delete(cacheName);
         }
-      }
+      };
 
       self.addEventListener('install', async (e) => {
-        const oldCacheData = await getOldCacheData()
+        const oldCacheData = await getOldCacheData();
+        const requestsToCache = [
+          ...oldCacheData,
+          ...routes.map(({ route }) => new Request(route)),
+        ];
 
-        e.waitUntil(caches.open(CACHE_VERSION).then(cache => {
-          return cache.addAll(new Set([...oldCacheData, ...routes.map(({ route }) => new Request(route))]))
-            .then(() => removeOldCaches())
+        e.waitUntil(caches.open(CACHE_VERSION).then((cache) => {
+          return cache.addAll(requestsToCache)
+            .then(() => removeOldCaches());
         }));
       });
     </code>
