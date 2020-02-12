6f5939a26f2cca23a8c373e1388a502ff0539ba9
shailen <shailen@lumkani.com>
2020-02-12 08:11:36 +0200

    Trying out new cache versioning

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,16 @@
+const removeOldCaches = async () => {
+  const cacheVersions = await caches.keys();
+
+  cacheVersions.splice(cacheVersions.indexOf(CACHE_VERSION), 1);
+
+  for (cacheName of cacheVersions) {
+    await caches.delete(cacheName)
+  }
+}
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('cache').then((cache) => {
-    return cache.addAll(routes.map(({ route }) => route));
+  e.waitUntil(caches.open(CACHE_VERSION).then((cache) => {
+    return cache.addAll(routes.map(({ route }) => route))
+      .then(() => removeOldCaches());
   }));
 });
