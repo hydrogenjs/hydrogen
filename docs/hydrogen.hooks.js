79999113abd2112d192a4b7249044a16ec1af549
shailen <shailen@lumkani.com>
2020-02-14 20:15:00 +0200

    Added build hooks

---
+++ docs/hydrogen.hooks.js
@@ -0,0 +1,28 @@
+exports.beforeDistRemoved = async (ctx) => {
+  // console.log(ctx);
+};
+
+exports.afterDistRemoved = async (ctx) => {
+};
+
+exports.beforeBuild = async (ctx) => {
+};
+
+exports.afterBuild = async (ctx) => {
+};
+
+exports.beforeEachPage = async (ctx) => {
+  
+};
+
+exports.afterEachPage = async (ctx) => {
+  console.log(ctx);
+};
+
+// exports.beforeServiceWorkerGenerated = async (ctx) => {
+//   console.log(ctx);
+// };
+
+// exports.afterServiceWorkerGenerated = async (ctx) => {
+//   console.log(ctx);
+// };
