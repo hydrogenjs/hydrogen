04cf17d731f84cca97e0dc3e923b93d0b881493c
shailen <shailen@lumkani.com>
2020-02-15 14:24:44 +0200

    Testing hook

--- docs/hydrogen.hooks.js
+++ docs/hydrogen.hooks.js
@@ -1,15 +1,3 @@
-exports.beforeDistRemoved = async (ctx) => {};
-
-exports.afterDistRemoved = async (ctx) => {};
-
-exports.beforeBuild = async (ctx) => {};
-
-exports.afterBuild = async (ctx) => {};
-
-exports.beforeEachPageGenerated = async (ctx) => {};
-
-exports.afterEachPageGenerated = async (ctx) => {};
-
-exports.beforeServiceWorkerGenerated = async (ctx) => {};
-
-exports.afterServiceWorkerGenerated = async (ctx) => {};
+exports.afterBuild = () => {
+  console.log('Finished build')
+};
