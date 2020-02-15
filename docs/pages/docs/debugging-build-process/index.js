2699fc8d7b2b65844c7a59a23aabcee953126725
shailen <shailen@lumkani.com>
2020-02-15 13:58:26 +0200

    Wrote docs for debugging a process

--- docs/pages/docs/debugging-build-process/index.js
+++ docs/pages/docs/debugging-build-process/index.js
@@ -14,25 +14,80 @@ const page = () => html`
   <p>
     Chrome has a built-in tool called the Node.js V8 inspector which allows us to debug Node.js applications in Chrome Devtools
   </p>
-  <small>Run the command below</small>
+  <p>1. Run the command below in the project root</p>
   <pre>
     <code class="lang-shell">
       npx --node-arg="--inspect-brk" hydrogen build --dev
     </code>
   </pre>
-  <small>You will output like this</small>
+  <p>2. You will see output like this</p>
   <pre>
     <code class="lang-text">
       Debugger listening on ws://127.0.0.1:9229/f35613dd-25b2-414f-a81f-24be123d59e5
       For help, see: https://nodejs.org/en/docs/inspector
     </code>
   </pre>
-  <p>Open the Chrome dev tools and you will see the Node logo popup right next to the Elements tab, click on it and you will go to the Node devtools</p>
+  <p>3. Open the Chrome dev tools and you will see the Node logo popup right next to the Elements tab, click on it and you will go to the Node devtools</p>
   <img src="/public/images/node-dev-tools.png">
-  <p>Once the Node Devtools are open, you will need to add your project folder to the workspace by clicking on "Add folder to workspace"</p>
+  <p>4. Once the Node Devtools are open, you will need to add your project folder to the workspace by clicking on "Add folder to workspace"</p>
   <img src="/public/images/node-inspector.png">
   <p>Now you can debug your JavaScript</p>
   <h2 id="using-synchronous-data">Debugging in VS Code</h2>
+  <p>
+    There are two ways we could setup debugging in VS Code,
+    either with VS Code's auto attaching feature or with a <code class="code">launch.json</code> file
+  </p>
+  <h3>Auto attaching</h3>
+  <p>
+    1. Go to settings and search "auto attach" and turn the setting on
+  </p>
+  <img src="/public/images/auto-attach.png">
+  <p>
+    2. Set a breakpoint anywhere in your JavaScript
+  </p>
+  <p>
+    3. Run the command below
+  </p>
+  <pre>
+    <code class="lang-shell">
+      npx --node-arg="--inspect-brk" hydrogen build --dev
+    </code>
+  </pre>
+  <p>
+    VS Code will automatically attach to the Node.js debugging process and stop by your breakpoint
+  </p>
+  <h3>Setting up <code class="code">launch.json</code></h3>
+  <p>
+    1. Go to the <i>Debug Explorer</i> and click on the <i>cogwheel</i> on the top right, it will open up a <code class="code">launch.json</code> file
+  </p>
+  <img src="/public/images/vscode-debug.png">
+  <p>
+    2. Copy and paste the below JSON config to your launch.json file
+  </p>
+  <pre>
+    <code class="lang-json">
+      {
+        "version": "0.2.0",
+        "configurations": [
+          {
+            "type": "node",
+            "request": "launch",
+            "name": "Debug Hydrogen CLI",
+            "skipFiles": [
+              "<node_internals>/**"
+            ],
+            "program": "$n{workspaceFolder}/node_modules/hydrogen-cli/bin/run",
+            "cwd": "$n{workspaceFolder}",
+            "args": ["build", "--dev"],
+          }
+        ]
+      }
+    </code>
+  </pre>
+  <p class="tip">Make sure to set a breakpoint</p>
+  <p>
+    3. Go back to the Debug Explorer and run the debug "Debug Hydrogen CLI"
+  </p>
 `.replace(/\$n/g, '$');
 
 module.exports = {
