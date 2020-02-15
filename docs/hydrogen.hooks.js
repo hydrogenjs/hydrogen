4bd7f3c6b2dbfaae44857036e42bd0409e76a99c
shailen <shailen@lumkani.com>
2020-02-15 09:17:11 +0200

    Found a way to debug hydrogen process

--- docs/hydrogen.hooks.js
+++ docs/hydrogen.hooks.js
@@ -3,26 +3,29 @@ exports.beforeDistRemoved = async (ctx) => {
 };
 
 exports.afterDistRemoved = async (ctx) => {
+  // console.log(ctx);
 };
 
 exports.beforeBuild = async (ctx) => {
+  // console.log(ctx);
 };
 
 exports.afterBuild = async (ctx) => {
+  // console.log(ctx);
 };
 
 exports.beforeEachPage = async (ctx) => {
-  
+  console.log(ctx.page.path);
 };
 
 exports.afterEachPage = async (ctx) => {
-  console.log(ctx);
+  // console.log(ctx);
 };
 
-// exports.beforeServiceWorkerGenerated = async (ctx) => {
-//   console.log(ctx);
-// };
+exports.beforeServiceWorkerGenerated = async (ctx) => {
+  // console.log(ctx);
+};
 
-// exports.afterServiceWorkerGenerated = async (ctx) => {
-//   console.log(ctx);
-// };
+exports.afterServiceWorkerGenerated = async (ctx) => {
+  // console.log(ctx);
+};
