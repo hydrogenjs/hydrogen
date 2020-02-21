facc6f9f0b352702802e48aceb9f7744e3b126a0
shailen <shailen@lumkani.com>
2020-02-21 19:14:46 +0200

    Cleaned up index page

--- docs/hydrogen.config.js
+++ docs/hydrogen.config.js
@@ -8,7 +8,7 @@ module.exports = {
   head: ({ config }) => [
     ['link', { rel: 'stylesheet', href: `/${config.staticFolder}/css/main.css` }],
     ['link', { rel: 'stylesheet', href: 'https://highlightjs.org/static/demo/styles/tomorrow-night-bright.css' }],
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Palanquin&display=swap' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Muli&display=swap' }],
     ['script', { src: 'https://yandex.st/highlightjs/8.0/highlight.min.js' }, true],
     ['script', {}, 'hljs.initHighlightingOnLoad();'],
     ['script', { src: `/${config.staticFolder}/js/script.js` }, true],
